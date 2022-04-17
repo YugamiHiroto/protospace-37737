50d249c78d757b9b7e97b9965a5105cdd18715a6
YugamiHitoro <hiro_0923_nk@yahoo.co.jp>
2022-04-17 11:30:46 +0900

    エラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,5 @@
 class PrototypesController < ApplicationController
+  before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
@@ -59,6 +60,10 @@ class PrototypesController < ApplicationController
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+  
   def contributor_confirmation
     redirect_to root_path unless current_user == @prototype.user
   end
